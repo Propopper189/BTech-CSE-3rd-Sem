b7ff09c0372490fd6f75f7e47b7e99ebcb003e51
akxeeb <jawaidaquib893@gmail.com>
2025-10-27 16:57:32 +0530

    updating oops

--- OOPS/Unit 4/inheritance.cpp
+++ OOPS/Unit 4/inheritance.cpp
@@ -6,6 +6,12 @@ class person
     public:
         string name;
         int age;
+        person(string n, int a)
+        {
+            cout<<"Person class constructor\n";
+            this->name = n;
+            this->age = a;
+        }
         void printperson()
         {
             cout<<name<<" "<<age<<endl;
@@ -17,6 +23,12 @@ class person
         public:
         string course;
         float gpa;
+        student(string n, int a, string c, float g) : person(n, a)
+        {
+            cout<<"Student class constructor\n";
+            course = c;
+            gpa = g;
+        }
         void printStudent()
         {
             cout<<name<<" "<<age<<endl;
@@ -26,11 +38,11 @@ class person
 
 int main()
 {
-    student obj1;
-    obj1.name = "Raj";
-    obj1.age = 21;
-    obj1.course = "B.Tech CSE";
-    obj1.gpa = 9.1;
+    student obj1("Raj", 32, "B.Tech CSE", 9.1);
+    // obj1.name = "Raj";
+    // obj1.age = 21;
+    // obj1.course = "B.Tech CSE";
+    // obj1.gpa = 9.1;
     obj1.printStudent();
     // cout<<obj1.name<<endl<<obj1.age<<endl<<obj1.course<<endl<<obj1.gpa<<endl;
     return 0;
