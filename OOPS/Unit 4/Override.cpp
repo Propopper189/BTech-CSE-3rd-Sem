735834eaa48339aac7beb4217431d4e3b15c0bee
akxeeb <jawaidaquib893@gmail.com>
2025-10-29 14:32:23 +0530

    Updating OOPS

---
+++ OOPS/Unit 4/Override.cpp
@@ -0,0 +1,29 @@
+// Override, Virtual
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+    public:
+        virtual void sound()
+        {
+            cout<<"Animal Is Making Sound\n";
+        }
+};
+
+class Dog : public Animal
+{
+    public:
+        void sound() override
+        {
+            cout<<"Dog Is Barking\n";
+        }
+};
+int main()
+{
+    Dog gr;
+    Animal *ptr = &gr;
+    gr.sound();
+    ptr->sound();
+    return 0;
+}
