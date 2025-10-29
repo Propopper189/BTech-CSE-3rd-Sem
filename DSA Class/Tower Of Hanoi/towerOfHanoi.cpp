714ff825fb07458242834c30cdecae8dabc5258a
akxeeb <jawaidaquib893@gmail.com>
2025-10-29 15:48:17 +0530

    Uploading Tower Of Hanoi

---
+++ DSA Class/Tower Of Hanoi/towerOfHanoi.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void towerOfHanoi(int n, char source, char destination, char auxiliary)
+{
+    if(n == 1)
+    {
+        cout<<"Move disk from "<<source<<" to "<<destination<<endl;
+        return;
+    }
+    towerOfHanoi(n-1, source, auxiliary, destination);
+    cout<<"Move disk from "<<source<<" to "<<destination<<endl;
+    towerOfHanoi(n-1, auxiliary, destination, source);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    // A -> Source
+    // B -> Auxiliary
+    // C -> Destinartion
+    towerOfHanoi(n, 'A', 'C', 'B');
+    return 0;
+}
