2ce8a45000f17d7960373fa7559a5ee473f5f48d
akxeeb <jawaidaquib893@gmail.com>
2025-11-27 14:26:53 +0530

    Updating Exception Handling

---
+++ OOPS/Unit 6/exception.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout<<10/0;
+    // int arr[10000000000];
+    // int *arr = new int[1000000000000000];
+    // cout<<12;
+    int a, b;
+    cin>>a>>b;
+    try
+    {
+        if(b == 0)
+        {
+            throw "division by zero";
+        }
+        cout<<a/b<<endl;
+    }catch(const char *c)
+    {
+        cout<<"Error: "<<c<<endl;
+    }
+    cout<<"Program executed!!"<<endl;
+    return 0;
+}
