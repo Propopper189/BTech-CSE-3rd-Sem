9d28eb48aec5dc992d4c1556b6ef9bcfaee92b3e
akxeeb <jawaidaquib893@gmail.com>
2025-11-27 15:54:25 +0530

    Updating

--- OOPS/Unit 6/badAlloc.cpp
+++ OOPS/Unit 6/badAlloc.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
 #include <exception>
 using namespace std;
+
+int divide(int a, int b)
+{
+        try
+        {
+            if(b == 0)
+            {
+                throw logic_error("Zero\n");
+            }
+            return a/b;
+        }
+        catch(...)
+        {
+            cout<<"something went wrong\n";
+            throw;
+        }
+}
 int main()
 {
+    // try
+    // {
+    //     // int *arr = new int[10000000000000000];
+    //     throw logic_error("test error");
+    // }catch(bad_alloc &err)
+    // {
+    //     cout<<err.what();
+    // }
+    int a, b;
+    cin>>a>>b;
     try
     {
-        // int *arr = new int[10000000000000000];
-        throw logic_error("test error");
-    }catch(bad_alloc &err)
+        cout<<divide(a, b)<<endl;
+    }
+    catch(exception &err)
     {
-        cout<<err.what();
+        cout<<"Error: "<<err.what()<<endl;
     }
     cout<<"\nProgram executed!"<<endl;
     return 0;
