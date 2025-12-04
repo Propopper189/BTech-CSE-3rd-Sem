a4df9659eec7832f8a50c3eb79a630d2dd3b9ac3
akxeeb <jawaidaquib893@gmail.com>
2025-12-04 14:22:45 +0530

    Updating OOPS

--- OOPS/Unit 6/typename.cpp
+++ OOPS/Unit 6/typename.cpp
@@ -7,14 +7,32 @@ class Array
     public:
         T *arr;
         int size;
-        Array(T arr[], int n)
-        {
-            size = n;
-            this->arr = new T;
-        }
+        Array(T arr[], int n); // just declare the function inside the class for creating a non inline function
+        void print(); // just declare the function inside the class for creating a non inline function
 };
+template <typename T>
+Array<T>::Array(T arr[], int n)
+{
+    size = n;
+    this->arr = new T[size];
+    for(int i =0; i < size; i++)
+    {
+        this->arr[i] = arr[i];
+    }
+}
+
+template <typename T>
+void Array<T>::print()
+{
+    for(int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
 int main()
 {
-
+    int arr[] = {1, 2, 3, 4, 5};
+    Array<int> a(arr, 5);
+    a.print();
     return 0;
 }
