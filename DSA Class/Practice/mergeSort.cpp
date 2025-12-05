2af00f0f93674d72a8667480e0a6d228259d5716
akxeeb <jawaidaquib893@gmail.com>
2025-12-05 14:55:13 +0530

    Updating

--- DSA Class/Practice/mergeSort.cpp
+++ DSA Class/Practice/mergeSort.cpp
@@ -3,16 +3,10 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i = 0; i < n; i++)
+    int *arr = new int[100];
+    for(int i = 0; i < 101 + 2; i++)
     {
-        cin>>arr[i];
-    }
-    for(int i = 0; i < n; i++)
-    {
-        cout<<arr[i]<<" ";
+        cout<<i<<" ";
     }
     return 0;
 }
