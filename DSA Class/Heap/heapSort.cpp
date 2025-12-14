c8f407475bc5fa578178aa8765e1e499dfea15db
akxeeb <jawaidaquib893@gmail.com>
2025-12-14 22:13:58 +0530

    Updating

---
+++ DSA Class/Heap/heapSort.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+void heapify(int *arr, int n, int i)
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    if(left < n && arr[largest] < arr[left])
+    {
+        largest = left;
+    }
+    if(right < n && arr[largest] < arr[right])
+    {
+        largest = right;
+    }
+    if(largest != i)
+    {
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+void heapSort(int *arr, int n)
+{
+    for(int i = (n/2)-1; i >= 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+    for(int i = n-1; i >= 1; i--)
+    {
+        swap(arr[i], arr[0]);
+        heapify(arr, i, 0);
+    }
+}
+
+void display(int *arr, int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int *arr = new int[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    heapSort(arr, n);
+    display(arr, n);
+    delete [] arr;
+    return 0;   
+}
